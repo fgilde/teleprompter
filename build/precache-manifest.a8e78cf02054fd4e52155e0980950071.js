bf454e4f1a5d27a11d4af003da17b64e3cb17f3a
Florian Gilde <florian.gilde@sabio.de>
2020-11-30 14:07:27 +0100

    test

---
+++ build/precache-manifest.a8e78cf02054fd4e52155e0980950071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eecda4bf19b2b43e3d1864c9a17b6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79f8ea2b177b0bd9dcf7",
+    "url": "/static/js/2.ba88b9fc.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/static/js/2.ba88b9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d936762ded012fc8270",
+    "url": "/static/js/main.7d3be549.chunk.js"
+  },
+  {
+    "revision": "58cb158aa2d546ca4898",
+    "url": "/static/js/runtime-main.33f659d6.js"
+  }
+]);
