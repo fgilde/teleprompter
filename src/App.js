cd1a9c6ee784d28a5025e05fbbf8d368fb58e700
Florian Gilde <florian.gilde@sabio.de>
2020-11-30 13:56:37 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, {
+  Fragment
+} from 'react'
+import {
+  GlobalStyles,
+  StyledApp,
+  StyledTeleprompter as Teleprompter,
+  Controls,
+  Buttons,
+  Input,
+  Button,
+  ButtonRight
+} from './styles'
+
+const INITIAL_TEXT = `Dies ist ein Beispiel und sie können den Text vorlesen und der text folgt dem was sie sagen. Beginnen sie einfach damit nachdem sie auf start geklickt haben diesen Text zu lesen und beobachten sie wie die Wörter hervorgehoben werden und wie ihnen der Text folgt.`
+
+export default function App() {
+  const [
+    listening,
+    setListening
+  ] = React.useState(false)
+  const [
+    words,
+    setWords
+  ] = React.useState(
+    INITIAL_TEXT.split(' ')
+  )
+  const [
+    progress,
+    setProgress
+  ] = React.useState(0)
+
+  const [
+    intervalValue,
+    setIntervalValue
+  ] = React.useState(200)
+
+  const handleInput = e => {
+    setWords(
+      e.target.value.split(' ')
+    )
+    progress && setProgress(0)
+  }
+
+  const handleListening = () => {
+    if (listening) {
+      setListening(false)
+      if (window.intervalId) {
+        clearInterval(window.intervalId);
+      }
+    } else {
+      setProgress(0)
+      setListening(true)
+    }
+  }
+
+  const handleIntervalValue = e => {
+   setIntervalValue(e.target.value)
+  }
+
+  const startStatic = () => {
+    handleListening();
+    var i = progress;
+    window.intervalId = setInterval(() => setProgress(i+=1), intervalValue);
+  }
+
+  const handleReset = () =>
+    setProgress(0)
+
+  const handleChange = progress =>
+    setProgress(progress)
+
+  return (
+    <Fragment>
+      <GlobalStyles />
+      <StyledApp>
+        <Controls>
+          <Input
+            onChange={handleInput}
+            value={words.join(' ')}
+          />
+
+          <Buttons>
+            <Button
+              onClick={
+                handleListening
+              }
+            >
+              {listening
+                ? 'Stop'
+                : 'Start dynamic'}
+            </Button>
+
+            { listening ? null :
+             <Button
+              onClick={startStatic}
+              disabled={listening}
+            >
+              Start static
+            </Button>
+          }
+
+            <input type="text" onChange={handleIntervalValue} id="interval" value={intervalValue}></input> <p>MS</p>
+
+
+            <ButtonRight
+              onClick={handleReset}
+              disabled={listening}
+              secondary
+            >
+              Reset
+            </ButtonRight>
+
+
+          </Buttons>
+        </Controls>
+        <Teleprompter
+          words={words}
+          listening={listening}
+          progress={progress}
+          onChange={handleChange}
+        />
+      </StyledApp>
+    </Fragment>
+  )
+}
